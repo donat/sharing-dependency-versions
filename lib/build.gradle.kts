e5e05f53753e7ad0fb428bd579267caeef89842d
Donat Csikos <csdonat@gmail.com>
2020-09-24 09:42:56 +0200

    Define direct dependencies

---
+++ lib/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    `java-library`
+}
+repositories {
+    jcenter()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.+")
+    implementation("org.apache.commons:commons-math3:+")
+}
